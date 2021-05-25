13e655fdcf4d4ff1cd34b4a74809dc111d106c1d
prathamesh-g-7 <prathameshgaikwad13999@gmail.com>
2021-05-25 11:33:25 +0530

    ADD: detail page

--- src/App.js
+++ src/App.js
@@ -3,12 +3,25 @@ import { Counter } from "./features/counter/Counter";
 import "./App.css";
 import Header from "./components/Header";
 import Home from "./components/Home";
+import Details from "./components/Details";
+import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <Header />
-      <Home />
+      <Router>
+        <Header />
+
+        <Switch>
+          <Route exact path="/details">
+            <Details />
+          </Route>
+
+          <Route exact path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
